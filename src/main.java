e053502fe85a9c0d6f800a51981c93ffe782f2e1
TaMeChel <148263615+TaMeChel@users.noreply.github.com>
2023-10-18 23:10:26 +0500

    Домашнее задание №2

--- src/main.java
+++ src/main.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 
 public class main {
-    public static void main (String[] args){
+    public static void main (String args []){
         Scanner scanner = new Scanner(System.in);
         System.out.print ("Введите первое число: ");
         int x = scanner.nextInt();
         System.out.print ("Введите второе число: ");
         int y = scanner.nextInt();
+        System.out.printf ("Результат сложения: ");
         int rez1 = x + y;
+        System.out.println (rez1);
+        System.out.printf ("Результат вычитания: ");
         int rez2 = x - y;
+        System.out.println (rez2);
+        System.out.printf ("Результат умножения: ");
         int rez3 = x * y;
+        System.out.println (rez3);
+        System.out.printf ("Результат деления: ");
         int rez4 = x / y;
-        System.out.print ("Результат сложения: %d, результат вычитания: %d, результат умножения: %d, результат деления: %d", rez1, rez2, rez3, rez4);
+        System.out.println (rez4);
         scanner.close();
     }
 }
